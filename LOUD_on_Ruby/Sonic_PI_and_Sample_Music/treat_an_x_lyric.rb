6a70f12268ba69ea3e228b5cbb1bd4ddba9f7e0d
Isaiah Pone <147687746+brokenprince96@users.noreply.github.com>
2026-04-25 17:18:45 -0700

    Update treat_an_x_lyric.rb
    privacy

--- LOUD_on_Ruby/Sonic_PI_and_Sample_Music/treat_an_x_lyric.rb
+++ LOUD_on_Ruby/Sonic_PI_and_Sample_Music/treat_an_x_lyric.rb
@@ -1,6 +1,6 @@
 # Welcome to Sonic Pi
 in_thread do
-  sample "C:/Users/casse/source/projectRuby/Loud/ProjectMusicOST/samp1_8.wav", amp: 2
+  sample "C:/PATH", amp: 2
 end
 
 
@@ -9,35 +9,35 @@ in_thread do
   sync :qoo
   live_loop :ost do
     2.times do
-      sample "C:/Users/casse/source/projectRuby/Loud/ProjectMusicOST/samp1_9.wav", amp: 2.2, beat_stretch: 1.9
+      sample "C:/PATH", amp: 2.2, beat_stretch: 1.9
       sleep 1.6
     end
     2.times do
-      sample "C:/Users/casse/source/projectRuby/Loud/ProjectMusicOST/samp1_5.wav", amp: 2.2, beat_stretch: 1.9
+      sample "C:/PATH", amp: 2.2, beat_stretch: 1.9
       sleep 1.6
     end
     2.times do
-      sample "C:/Users/casse/source/projectRuby/Loud/ProjectMusicOST/samp1_9.wav", amp: 2.2, beat_stretch: 1.9
+      sample "C:/PATH", amp: 2.2, beat_stretch: 1.9
       sleep 1.6
     end
     2.times do
-      sample "C:/Users/casse/source/projectRuby/Loud/ProjectMusicOST/samp1_5.wav", amp: 2.2, beat_stretch: 1.9
+      sample "C:/PATH", amp: 2.2, beat_stretch: 1.9
       sleep 1.6
     end
     2.times do
-      sample "C:/Users/casse/source/projectRuby/Loud/ProjectMusicOST/samp1_9.wav", amp: 2.2, beat_stretch: 1.9
+      sample "C:/PATH", amp: 2.2, beat_stretch: 1.9
       sleep 1.6
     end
     2.times do
-      sample "C:/Users/casse/source/projectRuby/Loud/ProjectMusicOST/samp1_5.wav", amp: 2.2, beat_stretch: 1.9
+      sample "C:/PATH", amp: 2.2, beat_stretch: 1.9
       sleep 1.6
     end
     2.times do
-      sample "C:/Users/casse/source/projectRuby/Loud/ProjectMusicOST/samp1_9.wav", amp: 2.2, beat_stretch: 1.9
+      sample "C:/PATH", amp: 2.2, beat_stretch: 1.9
       sleep 1.6
     end
     1.times do
-      sample "C:/Users/casse/source/projectRuby/Loud/ProjectMusicOST/samp1_15.wav", amp: 1.5, beat_stretch: 11
+      sample "C:/PATH", amp: 1.5, beat_stretch: 11
       sleep 10
     end
     
