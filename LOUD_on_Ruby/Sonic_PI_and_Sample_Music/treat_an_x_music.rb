ee5c87fa5a1e89495ee139e7407fe7d8128d228a
Isaiah Pone <147687746+brokenprince96@users.noreply.github.com>
2026-04-25 17:17:12 -0700

    Update treat_an_x_music.rb
    privacy

--- LOUD_on_Ruby/Sonic_PI_and_Sample_Music/treat_an_x_music.rb
+++ LOUD_on_Ruby/Sonic_PI_and_Sample_Music/treat_an_x_music.rb
@@ -15,19 +15,19 @@ end
 
 in_thread do
   sync :ooo
-  sample "C:/Users/casse/source/projectRuby/Loud/ProjectMusicOST/part1_future.wav", amp: 2
+  sample "C:/PATH", amp: 2
   
 end
 
 in_thread do
   sync :moo
-  sample "C:/Users/casse/source/projectRuby/Loud/ProjectMusicOST/part2_future.wav", amp: 2
+  sample "C:/PATH", amp: 2
 end
 
 in_thread do
   sync :voo
   live_loop :ost3 do
-    sample "C:/Users/casse/source/projectRuby/Loud/ProjectMusicOST/samp1_20.wav", beat_stretch: 16, amp: 1.3
+    sample "C:/PATH", beat_stretch: 16, amp: 1.3
     sleep 15.6
   end
 end
@@ -35,19 +35,12 @@ end
 in_thread do
   sync :noo
   live_loop :ost2 do
-    sample "C:/Users/casse/source/projectRuby/Loud/ProjectMusicOST/samp1_18.wav", rate: 0.8, amp: 1.6, threshold: 4
+    sample "C:/PATH", rate: 0.8, amp: 1.6, threshold: 4
     sleep 3
     
   end
   
 end
-
-=begin
-  live_loop :ost3 do
-    sample "C:/Users/casse/source/projectRuby/Loud/ProjectMusicOST/samp1_14.wav", beat_stretch: 5, amp: 1.3
-    sleep 5
-  end
-=end
 
 sleep 1
 set :coo, 1
